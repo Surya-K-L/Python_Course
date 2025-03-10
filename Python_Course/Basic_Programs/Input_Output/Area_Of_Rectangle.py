049c677a3b312359fcc2b467576f4fd6906a3ccc
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2025-03-10 09:43:34 +0530

    Basic_Python_Programs

---
+++ Python_Course/Basic_Programs/Input_Output/Area_Of_Rectangle.py
@@ -0,0 +1,8 @@
+# Write a Program to calculate the area of rectangle.
+
+# Note: Area of rectangle = Length * Breadth
+
+
+a,b=map(int,input().split())
+area=a*b
+print(f"Area of rectangle is {area}")
